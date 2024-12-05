8d0e18e37a728ef0fafa1ca901ad15714612fea3
lokesh <lokilokesh4031@gmail.com>
2024-12-05 06:46:45 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template
+
+flask_app = Flask(__name__)
+
+
+@flask_app.route('/')
+def index():
+    dic = {"name":"lokesh","mobile":"8919313027"}
+    return render_template('index.html',context=dic)
+
+
+
+@flask_app.route('/about-us')
+def about_us():
+    return render_template('about.html')
+
+@flask_app.route('/contact-us')
+def contact_us():
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    flask_app.run(
+        host='127.0.0.1',
+        port=5000,
+        debug=True
+    )
